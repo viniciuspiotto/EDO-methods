e2c4a9c6e57d9122dd709b1b362fc37ee05fe837
MaiconMian <maicon.mian@sou.unifal-mg.edu.br>
2025-08-13 14:42:24 -0300

    feat: adding quartile error

--- include/error.h
+++ include/error.h
@@ -2,6 +2,7 @@
 #define ERROR_H
 #include "methods.h"
 
-double error(Result res, Result correct);
+double * error (Result res, Result correct);
+double iterationError (Result res, Result correct, int start, int end);
 
 #endif
--- src/error.c
+++ src/error.c
@@ -4,29 +4,42 @@
 #include "error.h"
 #include "methods.h"
 
-double error (Result res, Result correct) {
+double iterationError(Result res, Result correct, int start, int end){
 
     double soma = 0.0;
     double max_val = 0.0;
 
-    for (int i = 0; i < correct.size; i++) {
+    for (int i = start; i < end; i++) {
         double val = fabs(correct.y_values[i]);
         if (val > max_val) {
             max_val = val;
         }
     }
 
-    for (int i = 0; i < res.size; i++) {
+    for (int i = start; i < end; i++) {
         double diff = res.y_values[i] - correct.y_values[i];
         soma += pow(diff, 2);
     }
 
-    double rms = sqrt(soma / res.size);
+    double rms = sqrt(soma / (end - start));
 
     if (max_val != 0.0) {
         return rms / max_val; 
     } else {
         return 0.0; 
     }
+
+}
+
+double * error(Result res, Result correct) {
+
+    double * errors = malloc(5 * sizeof(double));
+
+    for (int i = 0; i < 4; i++){
+       errors[i] = iterationError(res, correct, i * 5, (i != 3) ? (i + 1) * 5 : ((i + 1) * 5) + 1);
+    }
+
+    errors[4] = iterationError(res, correct, 0, 21);
+    return errors;
     
 }
